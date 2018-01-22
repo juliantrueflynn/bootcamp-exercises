f2ad808e520e017b107e2ca12eee04260f9731e1
Julian Flynn <hello@juliantrueflynn.com>
2018-01-22 16:42:31 -0500

    Add cursor class

--- w2d1/chess/board.rb
+++ w2d1/chess/board.rb
@@ -26,10 +26,16 @@ class Board
 	def move_piece(start_pos, end_pos)
 		start_row, start_col = start_pos
 		end_row, end_col = end_pos
+
 		raise ArgumentError.new("There's no piece in that spot!") if grid[start_row][start_col].nil?
 		raise ArgumentError.new("That's not a spot on the board!") unless grid[end_row][end_col].nil?
 
 		grid[end_row][end_col] = grid[start_row][start_col]
 		grid[start_row][start_col] = nil
+	end
+
+	def self.in_bounds?(pos)
+		row, col = pos
+		row.between?(0, 7) && col.between?(0, 7)
 	end
 end
--- w2d1/chess/display.rb
+++ w2d1/chess/display.rb
@@ -1,3 +1,12 @@
-class Display
+require "colorize"
+require_relative "cursor"
+require_relative "board"
 
+class Display
+	attr_reader :board
+
+	def initialize
+		@board = Board.new
+		@cursor = Cursor.new([0,0], @board)
+	end
 end
