1c623590255953193425205fa22185bc59022ae8
Julian Flynn <hello@juliantrueflynn.com>
2018-01-22 18:15:00 -0500

    Add colorize and cursor ability

--- w2d1/chess/display.rb
+++ w2d1/chess/display.rb
@@ -3,10 +3,48 @@ require_relative "cursor"
 require_relative "board"
 
 class Display
-	attr_reader :board
+	attr_reader :cursor
 
 	def initialize
 		@board = Board.new
 		@cursor = Cursor.new([0,0], @board)
 	end
+
+	def build_grid
+		@board.grid.map.with_index do |row, i|
+			build_row(row, i)
+		end
+	end
+
+	def build_row(row, i)
+		row.map.with_index do |piece, j|
+			color_options = colors_for(i, j)
+			piece.to_s.colorize(color_options)
+		end
+	end
+
+	def colors_for(i, j)
+		if cursor.cursor_pos == [i, j] && cursor.selected
+			bg = :light_green
+		elsif cursor.cursor_pos == [i, j]
+			bg = :light_red
+		elsif (i + j).odd?
+			bg = :light_blue
+		else
+			bg = :light_yellow
+		end
+		{ background: bg }
+	end
+
+	def render
+		system("clear")
+		puts "Arrow keys, WASD, or vim to move, space or enter to confirm"
+
+		build_grid.each { |row| puts row.join }
+	end
+end
+
+if __FILE__ == $PROGRAM_NAME
+	display = Display.new
+	display.render
 end
