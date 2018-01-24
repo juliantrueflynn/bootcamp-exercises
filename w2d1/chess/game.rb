b8b69188df0cd2c2630dbba3d13061603d9616f8
Julian <hello@juliantrueflynn.com>
2018-01-24 16:49:55 -0500

    Game renders/plays for tests

--- w2d1/chess/game.rb
+++ w2d1/chess/game.rb
@@ -1,3 +1,14 @@
-class Game
+require_relative "board"
+require_relative "display"
 
+class Game
+  attr_reader :board, :display
+  def initialize
+    @board = Board.new
+    @display = Display.new(@board)
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Game.new.display.render
 end
