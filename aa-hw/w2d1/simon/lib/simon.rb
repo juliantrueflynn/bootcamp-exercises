54f92a2199edf47d1b4d34e6c553d11f1f56a111
Julian <hello@juliantrueflynn.com>
2018-01-24 21:04:29 -0500

    Working game

--- aa-hw/w2d1/simon/lib/simon.rb
+++ aa-hw/w2d1/simon/lib/simon.rb
@@ -6,46 +6,65 @@ class Simon
   def initialize
 		@sequence_length = 1
 		@game_over = false
-		@seq = []
+    @seq = []
+    @players_seq = []
   end
 
   def play
     until game_over
       take_turn
+      system("clear")
     end
 
-    if game_over
-      game_over_message
-      reset_game
-    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
     show_sequence
     require_sequence
-    round_success_message
     
-    @sequence_length += 1
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
     add_random_color
+    
+    seq.each do |color|
+      puts color
+      sleep 1
+      system("clear")
+      sleep 0.75 
+    end
   end
 
   def require_sequence
+    puts "Put colors from sequence in order. Press enter after each color."
+    players_seq = []
+    seq.each do |color|
+      user_color = gets.chomp
 
+      unless user_color == color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
   end
 
   def add_random_color
-		@seq << COLORS.sample
+		@seq << COLORS.shuffle[0]
   end
 
   def round_success_message
-
+    puts "LEVEL PASSED!"
   end
 
   def game_over_message
-
+    puts "GAME OVER!"
   end
 
   def reset_game
@@ -53,4 +72,8 @@ class Simon
     @game_over = false
     @seq = []
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Simon.new.play
 end
