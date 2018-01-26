9fddd0af6144c23f2465ae88eef28bb4818c88c6
Julian <hello@juliantrueflynn.com>
2018-01-26 15:55:48 -0500

    Complete Towers of Hanoi unit test

--- w2d3/tdd/spec/tdd_spec.rb
+++ w2d3/tdd/spec/tdd_spec.rb
@@ -35,12 +35,73 @@ end
 
 describe "TowersOfHanoi" do
   subject(:game) { TowersOfHanoi.new }
+  let(:towers) { game.towers }
 
-  describe "#move" do
-    it "Moves last item from array (tower) and moves to new array (tower)"
+  describe "#towers" do
+    it "exposes game to @towers instance variable" do
+      ivar = game.instance_variable_get(:towers)
+      
+      expect(ivar).to be(ivar)
+    end
+
+    it "stores discs as integers" do
+      towers.each do |tower|
+        expect(tower.all { |disc| disc.is_a?(Integer) }).to be_truthy
+      end
+    end
+
+    it "stores as array of arrays (2d array)"
+      expect(towers).to be_an_instance_of(Array)
+      towers.each { |tower| expect(tower).to be_an_instance_of(Array) }
+    end
+  end
+  
+  describe "#initialize" do
+    it "starts with all discs in first tower" do
+      expects(towers.map(&:length)).to eq([3, 0, 0])
+    end
+
+    it "starts with correct pattern in first tower" do
+      expects(towers.first).to eq([3, 2, 1])
+    end
+  end
+
+  describe "#move(from_tower, to_tower)" do
+    let!(:disc) { towers[0].last }
+    before(:each) { game.move(0, 1) }
+    
+    it "Pops disk from tower" do
+      expect(towers[0]).to eq([3, 2])
+    end
+
+    it "Pushes disk onto tower" do
+      expect(towers[1]).to eq([1])
+    end
+
+    it "does not affect towers uninvolved" do
+      expect(towers[2]).to be_empty
+    end
   end
 
   describe "#won?" do
-    it "Determine if towers are in winning pattern"
+    context "if all disks are in 2nd or last tower" do
+      it "return true" do
+        [[0, 1], [0, 2], [1, 2], [0, 1], [2, 0], [2, 1], [0, 1]].each do |move|
+          game.move(*move)
+        end
+
+        expect(game.won?).to be_truthy
+      end
+    end
+
+    context "if game is incomplete" do
+      it "return false" do
+        [[0, 1], [0, 2], [1, 2]].each do |move|
+          game.move(*move)
+        end
+
+        expect(game.won?).to be_falsey
+      end
+    end
   end
 end
