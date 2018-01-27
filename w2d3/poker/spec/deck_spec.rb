4ab7ace6e565b6b07d43d9dd0f5039ba075a5611
Julian <hello@juliantrueflynn.com>
2018-01-26 22:13:37 -0500

    Deck class unit test

--- w2d3/poker/spec/deck_spec.rb
+++ w2d3/poker/spec/deck_spec.rb
@@ -1,1 +1,26 @@
 require "deck"
+
+describe Deck do
+  subject(:deck) { Deck.new }
+  let(:cards) { deck.cards }
+
+  describe "#initialize" do
+    it "creates array of cards" do
+      ivar = deck.instance_variable_get(:@cards)
+      
+      expect(cards).to be(ivar)
+    end
+
+    it "starts with 52 cards" do
+      expect(cards.length).to be(52)
+    end
+  end
+
+  describe "#shuffle!" do
+    it "make deck unique each time" do
+      unshuffled = cards.dup
+      shuffled = deck.shuffle!
+      expect(unshuffled).not_to eq(shuffled)
+    end
+  end
+end
