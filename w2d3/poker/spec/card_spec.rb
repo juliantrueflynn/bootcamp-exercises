3165030e2ee5c319fa4279d45d68a01083f0f90f
Julian <hello@juliantrueflynn.com>
2018-01-27 10:59:03 -0500

    Refactor card unit test

--- w2d3/poker/spec/card_spec.rb
+++ w2d3/poker/spec/card_spec.rb
@@ -2,19 +2,42 @@ require "card"
 require "rspec"
 
 describe Card do
-  subject(:card) { Card.new }
+  subject(:card) { Card.new(:spades, :ten) }
 
   describe "#initialize" do
-    it "exposes instance variable rank" do
-      ivar = card.instance_variable_get(:@rank)
-
-      expect(card.rank).to be(ivar)
+    it "sets up card correctly" do
+      expect(card.suit).to eq(:spades)
+      expect(card.rank).to eq(:ten)
     end
 
-    it "exposes instance variable suit" do
-      ivar = card.instance_variable_get(:@suit)
+    it "raises error with invalid suit" do
+      expect do
+        Card.new(:test, :ten)
+      end.to raise_error
+    end
 
-      expect(card.suit).to be(ivar)
+    it "raises an error with an invalid value" do
+      expect do
+        Card.new(:spades, :ten)
+      end.to raise_error
+    end
+  end
+
+  describe "#<=>" do
+    it "returns 0 when cards are the same" do
+      expect(Card.new(:spades, :ten) <=> Card.new(:spades, :ten)).to eq(0)
+    end
+
+    it "returns 1 when card has higher value" do
+      expect(Card.new(:spades, :ace) <=> Card.new(:spades, :ten)).to eq(1)
+    end
+
+    it "returns -1 when card has lower value" do
+      expect(Card.new(:spades, :ten) <=> Card.new(:spades, :ace)).to eq(-1)
+    end
+
+    it "returns -1 when card has same value but lower suit" do
+      expect(Card.new(:hearts, :ace) <=> Card.new(:spades, :ace)).to eq(-1)
     end
   end
 end
