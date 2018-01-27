34f622a8b3943edb9da1395954e354ee1e75c9a2
Julian <hello@juliantrueflynn.com>
2018-01-26 20:17:38 -0500

    Card class unit test

--- w2d3/poker/spec/card_spec.rb
+++ w2d3/poker/spec/card_spec.rb
@@ -1,1 +1,19 @@
 require "card"
+
+describe Card do
+  subject(:card) { Card.new }
+
+  describe "#initialize" do
+    it "exposes instance variable rank" do
+      ivar = card.instance_variable_get(:@rank)
+
+      expect(card.rank).to be(ivar)
+    end
+
+    it "exposes instance variable suit" do
+      ivar = card.instance_variable_get(:@suit)
+
+      expect(card.suit).to be(ivar)
+    end
+  end
+end
