406567f9a031f2c47f794c606c315758af37d02c
Julian <hello@juliantrueflynn.com>
2018-01-29 15:05:32 -0500

    Add #dominant_octopus for O(n log n)

--- aa-hw/w2d4/big_octopus.rb
+++ aa-hw/w2d4/big_octopus.rb
@@ -11,3 +11,31 @@ def sluggish_octopus(octopi)
     return octopus if longest
   end
 end
+
+def dominant_octopus(octopi)
+  return octopi if octopi.length <= 1
+
+  mid_idx = octopi.length / 2
+  merge_dominant_octopus(
+    dominant_octopus(octopi.take(mid_idx)),
+    dominant_octopus(octopi.drop(mid_idx)),
+  )
+end
+
+def merge_dominant_octopus(left, right)
+  merged_array = []
+  prc = Proc.new { |num1, num2| num2.length <=> num1.length }
+
+  until left.empty? || right.empty?
+    case prc.call(left.first, right.first)
+    when -1
+      merged_array << left.shift
+    when 0
+      merged_array << left.shift
+    when 1
+      merged_array << right.shift
+    end
+  end
+
+  merged_array
+end
