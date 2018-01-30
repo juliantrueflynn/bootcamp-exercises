39fa09fb51f5d279eae8bece023c1ee60fb4dbf0
Julian <hello@juliantrueflynn.com>
2018-01-29 23:31:07 -0500

    Complete LinkedList class

--- w2d5/hash_map/lib/p04_linked_list.rb
+++ w2d5/hash_map/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+include Enumerable
+
 class Node
   attr_accessor :key, :val, :next, :prev
 
@@ -20,38 +22,97 @@ end
 
 class LinkedList
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
-    each_with_index { |link, j| return link if i == j }
+    count = 0
+    current_node = first
+    until current_node == @tail
+      return current_node if i == count
+      current_node = current_node.next
+      count += 1
+    end
+
     nil
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    first == @tail
   end
 
   def get(key)
+    current_node = first
+    until current_node == @tail
+      return current_node.val if current_node.key == key
+      current_node = current_node.next
+    end
+
+    nil
   end
 
   def include?(key)
+    current_node = first
+    until current_node == @tail
+      return true if current_node.key == key
+      current_node = current_node.next
+    end
+
+    false
   end
 
   def append(key, val)
+    old_node = last
+    new_node = Node.new(key, val)
+    new_node.next = @tail
+    new_node.prev = last
+    old_node.next = new_node    
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    return unless include?(key)
+
+    current_node = first
+    until current_node == @tail
+      current_node.val = val if current_node.key == key
+      current_node = current_node.next
+    end
   end
 
   def remove(key)
+    return unless include?(key)
+
+    current_node = first
+    until current_node == @tail
+      if current_node.key == key
+        prev_node = current_node.prev
+        next_node = current_node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node        
+      end
+
+      current_node = current_node.next
+    end
   end
 
-  def each
+  def each(&prc)
+    current_node = first
+    until current_node == @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
