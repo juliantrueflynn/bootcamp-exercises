fd2762e9c812334b9cf4a07f849197cd7b5ccbfb
Julian <hello@juliantrueflynn.com>
2018-01-29 22:00:35 -0500

    Complete HashSet class

--- w2d5/hash_map/lib/p03_hash_set.rb
+++ w2d5/hash_map/lib/p03_hash_set.rb
@@ -1,3 +1,4 @@
+require "byebug"
 require_relative 'p02_hashing'
 
 class HashSet
@@ -9,18 +10,30 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key.hash)
+      self[key.hash] << key
+      @count += 1
+      resize! if @count == num_buckets
+    end
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
+    
+    key
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets] << num
   end
 
   def num_buckets
@@ -28,5 +41,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.each do |arr|
+      arr.each { |key| self[key.hash] << key }
+    end
   end
 end
