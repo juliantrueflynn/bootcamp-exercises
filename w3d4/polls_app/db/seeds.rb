a91d52d1ff9b68e14762b604f10e52b0c82309a8
Julian <hello@juliantrueflynn.com>
2018-02-06 22:17:09 -0500

    Import with seed file

--- w3d4/polls_app/db/seeds.rb
+++ w3d4/polls_app/db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Poll.destroy_all
+Question.destroy_all
+AnswerChoice.destroy_all
+Response.destroy_all
+
+ApplicationRecord.connection.reset_pk_sequence!('User')
+ApplicationRecord.connection.reset_pk_sequence!('Poll')
+ApplicationRecord.connection.reset_pk_sequence!('Question')
+ApplicationRecord.connection.reset_pk_sequence!('AnswerChoice')
+ApplicationRecord.connection.reset_pk_sequence!('Response')
+
+User.import(
+[:username],
+[['jtf2'],
+['rickie'],
+['gipoi']])
+
+Poll.import(
+[:user_id, :title],
+[[1, "Test1 poll by same author - 1"],
+[1, "Test2 poll by same author - 2"],
+[2, "New author #3 poll"],
+[3, "Last random poll"]])
+
+Question.import(
+[:text, :poll_id],
+[["Test1 Q to poll", 1],
+["Test2 Q to poll", 2],
+["New Q #3 poll", 3],
+["Last randomquestion", 4]])
+
+AnswerChoice.import(
+[:text, :question_id],
+[["sdfa sdfa sdfas", 1],
+["rtes rtsgv dfvsd", 2],
+["wera we casd", 3],
+["L sdfaswefa sdfn", 4]])
+
+Response.import(
+[:answer_choice_id, :respondent_id],
+[[1, 1],
+[2, 1],
+[3, 2],
+[4, 3]])
