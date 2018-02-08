1b11266f641b022eff481981e7ea526eda447d00
Julian <hello@juliantrueflynn.com>
2018-02-07 20:32:05 -0500

    Phase 2 complete

--- w3d5/lib/02_searchable.rb
+++ w3d5/lib/02_searchable.rb
@@ -1,12 +1,24 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require 'byebug'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map { |key, _| "#{key} = ?"}.join(" AND ")
+
+    data = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    data.map { |datum| self.new(datum) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  self.extend(Searchable)
 end
