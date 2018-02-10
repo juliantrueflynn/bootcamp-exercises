9dff678e041630b380ea9c2e3023f20bb7af60c7
Julian <hello@juliantrueflynn.com>
2018-02-09 22:51:43 -0500

    Init seed db file

--- w4d1/first_routes/db/seeds.rb
+++ w4d1/first_routes/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Artwork.destroy_all
+ArtworkShare.destroy_all
+
+ApplicationRecord.connection.reset_pk_sequence!('User')
+ApplicationRecord.connection.reset_pk_sequence!('Artwork')
+ApplicationRecord.connection.reset_pk_sequence!('ArtworkShare')
+
+User.create(username: 'jtf234')
+User.create(username: 'faaswera')
+User.create(username: 'blseeedsct')
+User.create(username: 'deeokkdc')
+User.create(username: 'helloid')
+User.create(username: 'userandaed')
+
+Artwork.create(title: "bladef", image_url: "http://fseassfe.com", artist_id: 1)
+Artwork.create(title: "sdgbladef", image_url: "http://fsfdsfe.com", artist_id: 1)
+Artwork.create(title: "blf", image_url: "http://fsege.com", artist_id: 1)
+Artwork.create(title: "gsdfsef", image_url: "http://asdfassfe.com", artist_id: 4)
+Artwork.create(title: "efef", image_url: "http://dfgadrr.com", artist_id: 3)
+Artwork.create(title: "bgtgttg", image_url: "http://cvbcfrrr.com", artist_id: 2)
+Artwork.create(title: "grrrtgttg", image_url: "http://cvbcfdr.com", artist_id: 5)
+
+ArtworkShare.create(artwork_id: 1, viewer_id: 4)
+ArtworkShare.create(artwork_id: 1, viewer_id: 3)
+ArtworkShare.create(artwork_id: 2, viewer_id: 2)
+ArtworkShare.create(artwork_id: 3, viewer_id: 1)
+ArtworkShare.create(artwork_id: 4, viewer_id: 5)
