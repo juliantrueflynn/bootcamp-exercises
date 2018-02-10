56226972aee28432592cce4cd036ba3f1e0cd20f
Julian <hello@juliantrueflynn.com>
2018-02-09 22:27:47 -0500

    ArtworkShare model assocations and migration

--- w4d1/first_routes/app/models/user.rb
+++ w4d1/first_routes/app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
     class_name: :Artwork,
     foreign_key: :artist_id,
     primary_key: :id
+  has_many :artwork_shares,
+    foreign_key: :viewer_id,
+    dependent: :destroy
+  has_many :shared_artworks,
+    through: :artwork_shares,
+    source: :artwork
 end
