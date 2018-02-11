7f576707863c932a087da28dd2d6f3d832d67c50
Julian <hello@juliantrueflynn.com>
2018-02-11 16:00:14 -0500

    Ability to create new Cat

--- w4d2/ninetynine_cats/app/controllers/cats_controller.rb
+++ w4d2/ninetynine_cats/app/controllers/cats_controller.rb
@@ -8,4 +8,23 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :show
   end
+
+  def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :birth_date, :sex, :color, :description)
+  end
 end
