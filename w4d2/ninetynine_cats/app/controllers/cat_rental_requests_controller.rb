ddf0119cc3bc4e289e51205b5815e8c957f18229
Julian <hello@juliantrueflynn.com>
2018-02-12 17:24:50 -0500

    Validate default params method for CatRentalRequest

--- w4d2/ninetynine_cats/app/controllers/cat_rental_requests_controller.rb
+++ w4d2/ninetynine_cats/app/controllers/cat_rental_requests_controller.rb
@@ -1,7 +1,12 @@
 class CatRentalRequestsController < ApplicationController
   def new
+
   end
 
   def create
+  end
+
+  def cat_rental_request_params
+    params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date, :status)
   end
 end
