66ea86fc721683160a37cbebf42944a94ba56bf5
Julian <hello@juliantrueflynn.com>
2018-02-12 16:51:24 -0500

    Methods to check if overlapping requests exist

--- w4d2/ninetynine_cats/app/models/cat_rental_request.rb
+++ w4d2/ninetynine_cats/app/models/cat_rental_request.rb
@@ -1,5 +1,20 @@
 class CatRentalRequest < ApplicationRecord
   validates :cat_id, :start_date, :end_date, :status, presence: true
   validates :status, inclusion: { in: %w(APPROVED DENIED) }
+  
   belongs_to :cat
+
+  def overlapping_requests
+    CatRentalRequest
+      .where(cat_id: cat_id)
+      .where.not(id: id)
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where(status: "APPROVED")
+  end
+  
+  def does_not_overlap_approved_request
+    ! overlapping_approved_requests.exists?
+  end
 end
