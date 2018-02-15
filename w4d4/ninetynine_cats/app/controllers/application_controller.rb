d7469f65eb838ddc539030f50b4e6439e6a9e606
Julian <hello@juliantrueflynn.com>
2018-02-15 14:20:47 -0500

    Helper method for current_user

--- w4d4/ninetynine_cats/app/controllers/application_controller.rb
+++ w4d4/ninetynine_cats/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_current_user!
+    redirect_to new_session_url if current_user.nil?
+  end
+
+  def current_user
+    return nil if session[:session_token].nil?
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  helper_method :current_user  
 end
