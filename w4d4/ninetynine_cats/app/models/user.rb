fdda9f46232ddea3c9cc8ee43162701a053cdd36
Julian <hello@juliantrueflynn.com>
2018-02-14 19:55:09 -0500

    User model session methods

--- w4d4/ninetynine_cats/app/models/user.rb
+++ w4d4/ninetynine_cats/app/models/user.rb
@@ -1,17 +1,39 @@
 class User < ApplicationRecord
+  require 'bcrypt'
+
   validates :username, :password_digest, :session_token, presence: true
   validates :username, uniqueness: true
-  validates :session_token, uniquness: true
+  validates :session_token, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
 
   def self.find_by_credentials(user_name, password)
+    user = self.find_by(username: user_name)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
   end
 
   def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
   end
 
   def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 end
