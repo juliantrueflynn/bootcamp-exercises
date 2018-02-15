90396d3d9fd8c12630214282e5f897bf79524a69
Julian <hello@juliantrueflynn.com>
2018-02-15 15:03:59 -0500

    User migration

--- aa-hw/w4d4/rails_auth/db/migrate/20180215192545_create_users.rb
+++ aa-hw/w4d4/rails_auth/db/migrate/20180215192545_create_users.rb
@@ -7,5 +7,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
+
+    add_index :users, :session_token, unique: true
+    add_index :users, :username, unique: true
   end
 end
