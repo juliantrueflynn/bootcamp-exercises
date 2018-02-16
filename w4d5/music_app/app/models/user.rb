1d6f1ff38a2f0c552e80ffcae190f155687a6e8d
Julian <hello@juliantrueflynn.com>
2018-02-16 13:33:17 -0500

    Add User model auth methods

--- w4d5/music_app/app/models/user.rb
+++ w4d5/music_app/app/models/user.rb
@@ -1,2 +1,43 @@
 class User < ApplicationRecord
+  require 'bcrypt'
+
+  attr_accessor :password
+
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(password)
+    password_hash = BCrypt::Password.create(password)
+    password_hash.is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)     
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
