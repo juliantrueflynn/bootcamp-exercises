b818557c8a6f9cefb5a8b5c4335275fef0629c39
Julian <hello@juliantrueflynn.com>
2018-02-16 16:05:03 -0500

    Add User#show REST action and init view

--- w4d5/music_app/app/controllers/users_controller.rb
+++ w4d5/music_app/app/controllers/users_controller.rb
@@ -9,11 +9,17 @@ class UsersController < ApplicationController
     
     if @user.save
       login!(@user)
-      redirect_to :new
+      redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    render :show
   end
 
   private
