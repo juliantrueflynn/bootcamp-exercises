b845a3a27f10680abe0acbebe89f584a444498ea
Julian <hello@juliantrueflynn.com>
2018-02-16 22:18:03 -0500

    Add Album#band_name helper method to model

--- w4d5/music_app/app/models/album.rb
+++ w4d5/music_app/app/models/album.rb
@@ -2,4 +2,9 @@ class Album < ApplicationRecord
   validates :band_id, :title, :year, presence: true
 
   belongs_to :band
+
+  def band_name
+    band = Band.find_by(id: band_id)
+    band.name
+  end
 end
