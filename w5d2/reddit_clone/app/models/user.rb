d80df06b6ca84ba654f04a8a84fc6551fdac7d31
Julian <hello@juliantrueflynn.com>
2018-02-19 14:42:58 -0500

    Add User model auth methods

--- w5d2/reddit_clone/app/models/user.rb
+++ w5d2/reddit_clone/app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
+  require 'bcrypt'
+
+  attr_reader :password
+
+  validates :username, :password_digest, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(username: user_name)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_hash = BCrypt::Password.new(self.password_digest)
+    password_hash.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
