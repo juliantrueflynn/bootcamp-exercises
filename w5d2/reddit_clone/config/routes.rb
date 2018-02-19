36c6955e391d035a00a6b03b96fb3e86b7b0d935
Julian <hello@juliantrueflynn.com>
2018-02-19 18:01:33 -0500

    Add routes for Session and Sub

--- w5d2/reddit_clone/config/routes.rb
+++ w5d2/reddit_clone/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :create, :new, :index]
+  resource :session
+  resources :subs, except: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
