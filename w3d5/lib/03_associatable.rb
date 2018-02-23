d161ba43e0b26eb8b992e806f6e4b3ce4c990588
Julian <hello@juliantrueflynn.com>
2018-02-22 22:37:55 -0500

    Complete project

--- w3d5/lib/03_associatable.rb
+++ w3d5/lib/03_associatable.rb
@@ -4,38 +4,27 @@ require 'byebug'
 
 # Phase IIIa
 class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
+  attr_accessor :foreign_key, :class_name, :primary_key
 
   def model_class
-    class_name.constantize
+    @class_name.constantize
   end
 
   def table_name
-    "#{class_name.downcase}s"
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     defaults = {
-      foreign_key: "#{name.singularize.underscore}_id".to_sym,
+      foreign_key: "#{name}_id".to_sym,
       primary_key: :id,
-      class_name: "#{name.camelcase}"
+      class_name: name.to_s.camelcase
     }
-    options = defaults.merge(options)
-
-    options.each do |attr_key, attr_val|
-      self.class.define_method(attr_key) do
-        attr_val
-      end
-
-      self.class.define_method("#{attr_key}=") do
-        self.class.attr_key = attr_val
-      end
+    
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
     end
   end
 end
@@ -43,20 +32,13 @@ end
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     defaults = {
-      foreign_key: "#{self_class_name.singularize.underscore}_id".to_sym,
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
       primary_key: :id,
       class_name: name.to_s.singularize.camelcase
     }
-    options = defaults.merge(options)
-
-    options.each do |attr_key, attr_val|
-      self.class.define_method(attr_key) do
-        attr_val
-      end
-
-      self.class.define_method("#{attr_key}=") do
-        self.class.attr_key = attr_val
-      end
+    
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
     end
   end
 end
@@ -64,19 +46,34 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    debugger
-    attr = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key_val).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
