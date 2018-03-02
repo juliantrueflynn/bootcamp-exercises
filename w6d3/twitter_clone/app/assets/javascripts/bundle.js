6fbb33a3a4b9c0d7015db7080460496657518aa3
Julian <hello@juliantrueflynn.com>
2018-03-02 16:52:27 -0500

    Complete phase 1

--- w6d3/twitter_clone/app/assets/javascripts/bundle.js
+++ w6d3/twitter_clone/app/assets/javascripts/bundle.js
@@ -65,9 +65,95 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const FollowToggle = __webpack_require__(1);
+
+$( () => {
+    $('button.follow-toggle').each((i, btn) => new FollowToggle(btn));
+});
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const APIUtil = __webpack_require__(2);
+
+class FollowToggle {
+    constructor(el) {
+        this.$el = $(el);
+        this.userId = this.$el.data('user-id');
+        this.followState = this.$el.data('initial-follow-state');
+
+        this.render();
+        this.$el.on('click', this.handleClick.bind(this));
+    }
+
+    render() {
+        switch (this.followState) {
+            case 'followed':
+                this.$el.prop('disabled', false);
+                this.$el.html('Unfollow!');
+                break;
+            case 'unfollowed':
+                this.$el.prop('disabled', false);
+                this.$el.html('Follow!');
+                break;
+            case 'following':
+                this.$el.prop('disabled', true);
+                this.$el.html('Following...');
+                break;
+            case 'unfollowing':
+                this.$el.prop('disabled', true);
+                this.$el.html('Unfollowing...');
+                break;
+        }
+      
+    }
+
+    handleClick(e) {
+        const followToggle = this;
+        
+        e.preventDefault();
+
+        if (this.followState === 'followed') {
+            this.followState = 'following';
+            this.render();
+            APIUtil.unfollowUser(this.userId).then(() => {
+                followToggle.followState = 'unfollowed';
+                followToggle.render();
+            });
+        } else if (this.followState === 'unfollowed') {
+            this.followState = 'following';
+            this.render();
+            APIUtil.followUser(this.userId).then(() => {
+                followToggle.followState = 'followed';
+                followToggle.render();
+            });
+        }
+    }
+}
+
+module.exports = FollowToggle;
+
+/***/ }),
+/* 2 */
 /***/ (function(module, exports) {
 
+const APIUtil = {
+    followUser: id => APIUtil.changeFollowStatus(id, 'POST'),
+    unfollowUser: id => APIUtil.changeFollowStatus(id, 'DELETE'),
 
+    changeFollowStatus: (id, method) => (
+        $.ajax({
+            url: `/users/${id}/follow`,
+            dataType: 'json',
+            method
+        })
+    )
+};
+
+module.exports = APIUtil;
 
 /***/ })
 /******/ ]);
