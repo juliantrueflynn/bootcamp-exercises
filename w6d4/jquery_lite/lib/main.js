2d81a32d976189d7644f460454997b6c307bcfad
Julian <hello@juliantrueflynn.com>
2018-03-05 23:08:29 -0500

    Hit time limit for project

--- w6d4/jquery_lite/lib/main.js
+++ w6d4/jquery_lite/lib/main.js
@@ -8,5 +8,40 @@ window.$l = (selector) => {
         arr.push(selector);
     }
 
+    if (selector instanceof Function) {
+        if (document.readyState === 'complete') {
+            console.log("loaded!");
+            return selector();
+        }
+    }
+
     return new DOMNodeCollection(arr);
+};
+
+window.$l.extend = (...objs) => {
+    let targetObj = objs.shift();
+    targetObj = Object.assign(targetObj, ...objs);
+    return targetObj;
+};
+
+window.$l.ajax = (options = {}) => {
+    let defaults = {
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        error: true,
+        success: true,
+        data: {},
+        method: 'GET',
+        url: window.location.href
+    };
+
+    options = Object.assign(defaults, options);
+
+    console.log(options);
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(options.method, options.url);
+
+    const optionalData = {};
+    xhr.send(optionalData);
 };
