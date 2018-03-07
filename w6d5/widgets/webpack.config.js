e5f55547a28ffc70d6b713e917a9fdb7c7b0e334
Julian <hello@juliantrueflynn.com>
2018-03-06 19:43:18 -0500

    Complete phase 1

--- w6d5/widgets/webpack.config.js
+++ w6d5/widgets/webpack.config.js
@@ -1,25 +1,26 @@
 var path = require('path');
 
 module.exports = {
-  entry: './index.html',
+  context: __dirname,
+  entry: "./widgets.jsx",
   output: {
-    path: __dirname,
-    filename: 'bundle.js'
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'es2015']
         }
       }
     ]
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '*']
+    extensions: [".js", '.jsx', '*']
   }
 };
---
+++ w6d5/widgets/widgets.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDom.render(<div></div>, document.getElementById('main'));
+});
