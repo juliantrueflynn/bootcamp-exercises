a2e318e2957434aa68b4f7e41e9e1f2946267cdf
Julian <hello@juliantrueflynn.com>
2018-03-06 19:21:55 -0500

    Init files

---
+++ w6d5/widgets/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './index.html',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
