8639a7af5f9b54f107ebe1b86e0a557a945c37c2
Julian <hello@juliantrueflynn.com>
2018-03-09 17:44:48 -0500

    Add selectors for todos

--- w7d1/todos_redux/frontend/todo_redux.jsx
+++ w7d1/todos_redux/frontend/todo_redux.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import store from './store/store';
 import Root from './components/root';
 
-import { allTodos } from './reducers/selectors';
-
 document.addEventListener('DOMContentLoaded', () => {
-  window.store = store;
-  window.allTodos = allTodos;
   ReactDOM.render(<Root store={ store } />, document.getElementById('content'));
 });
