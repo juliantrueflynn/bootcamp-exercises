73b1d85b8e6bc70d3a1cb6eb670f07c19590f749
Julian <hello@juliantrueflynn.com>
2018-03-09 17:44:22 -0500

    Add selectors for todos

--- w7d1/todos_redux/frontend/reducers/todos_reducer.js
+++ w7d1/todos_redux/frontend/reducers/todos_reducer.js
@@ -1,7 +1,22 @@
 import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
 import merge from 'lodash/merge';
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
   Object.freeze(state);
   let nextState;
 
