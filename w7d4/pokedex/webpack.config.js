d71642569316fd5abd51856b3adeb124dcf94a96
Julian <hello@juliantrueflynn.com>
2018-03-19 21:31:07 -0400

    Npm JS env and webpack config setup

---
+++ w7d4/pokedex/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/pokedex.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascript'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
