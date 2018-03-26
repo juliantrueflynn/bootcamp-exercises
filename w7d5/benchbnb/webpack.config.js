f9e304dcbf8d58ed33e83db66b8b745d06dd2b7c
Julian <hello@juliantrueflynn.com>
2018-03-26 15:18:27 -0400

    Init files setup

---
+++ w7d5/benchbnb/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/bench_bnb.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+};
