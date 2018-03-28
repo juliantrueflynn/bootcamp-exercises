3b25afe93a8c93302a3516293b4a219d02765f89
Julian <hello@juliantrueflynn.com>
2018-03-28 18:25:23 -0400

    Complete phase 3

--- w7d5/benchbnb/frontend/bench_bnb.jsx
+++ w7d5/benchbnb/frontend/bench_bnb.jsx
@@ -14,7 +14,15 @@ import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
-  const store = configureStore();
+
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } }
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   
   window.APIUtilSession = APIUtilSession;
   window.signup = signup;
